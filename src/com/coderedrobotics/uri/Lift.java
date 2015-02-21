34771364dd373ac5c167a62efc792b957ef70491
Michael Spoehr <spoehrmichael@gmail.com>
2015-02-21 17:35:38 -0500

    misc

--- src/com/coderedrobotics/uri/Lift.java
+++ src/com/coderedrobotics/uri/Lift.java
@@ -2,7 +2,7 @@ package com.coderedrobotics.uri;
 
 import com.coderedrobotics.libs.PIDControllerAIAO;
 import com.coderedrobotics.libs.PWMController;
-import com.coderedrobotics.libs.VirtualizableDigitalInput;
+import com.coderedrobotics.libs.VirtualizablePsuedoDigitalInput;
 import com.coderedrobotics.uri.statics.Calibration;
 import com.coderedrobotics.uri.statics.Wiring;
 import edu.wpi.first.wpilibj.Encoder;
@@ -16,7 +16,7 @@ public class Lift implements PIDOutput {
 
     private final PIDControllerAIAO pid;
     private final PWMController controller;
-    private final VirtualizableDigitalInput limitSwitch;
+    private final VirtualizablePsuedoDigitalInput limitSwitch;
     private final Encoder encoder;
     private boolean calibrated = false;
     private int calibration;
@@ -25,7 +25,7 @@ public class Lift implements PIDOutput {
     public Lift() {
         this.controller = new PWMController(Wiring.LIFT_MOTOR, false);
         encoder = new Encoder(Wiring.LIFT_ENCODER_A, Wiring.LIFT_ENCODER_B);
-        limitSwitch = new VirtualizableDigitalInput(Wiring.LIFT_LIMIT_SWITCH);
+        limitSwitch = new VirtualizablePsuedoDigitalInput(Wiring.LIFT_LIMIT_SWITCH);
         pid = new PIDControllerAIAO(Calibration.LIFT_P, Calibration.LIFT_I, 
                 Calibration.LIFT_D, encoder, this, "lift");
         maxDistance = Calibration.LIFT_MAX_DISTANCE;
@@ -39,13 +39,13 @@ public class Lift implements PIDOutput {
 
     private void move(double speed) {
         if (calibrated) {
-            if (!limitSwitch.get() && encoder.get() < calibration + maxDistance) {
+            if (!limitSwitch.getState() && encoder.get() < calibration + maxDistance) {
                 controller.set(speed);
             } else {
                 controller.set(0);
             }
         } else {
-            if (!limitSwitch.get() && speed < 0) {
+            if (!limitSwitch.getState() && speed < 0) {
                 controller.set(speed);
             } else {
                 controller.set(0);
@@ -58,7 +58,7 @@ public class Lift implements PIDOutput {
     }
 
     public boolean calibrate() {
-        if (!limitSwitch.get()) {
+        if (!limitSwitch.getState()) {
             move(-1);
             calibrated = false;
         } else {
